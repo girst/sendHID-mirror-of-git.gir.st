ae1c9f9572df8b4834fe0b809c57673560121581
girst <girst@users.noreply.github.com>
2016-02-11 18:32:11 +0100

    prepare for unicode support

--- main.c
+++ main.c
@@ -2,30 +2,39 @@
 description: sends a sequence of keystrokes to the hid device. 
 parameters:
 	device file (e.g. /dev/hidg0)
-	keyboard layout (e.g. en_us; only a limited number are supported and they do not follow unix-naming conventions for convenience)
-	the string to send (as whitespace is important, the `echo` way of concatenating all parameters is not supported. if your string has white space in it and you are in an interactive session, quote your string. 
+	keyboard layout (1=en_us, 2=de_at, 3=de_at-nodeadkeys)
+	unicode method: 1=gtk_holddown, 2=gtk_spaceend, 3=windows
+	the string to send (as whitespace is important, the `echo` way of concatenating all parameters is not supported. if your string has white space in it and you are in an interactive session, quote your string.)
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "scancodes.h"
 
+//argv-indices
+#define P_EXE 0 //executable name
+#define P_DEV 1 //device file
+#define P_LAY 2 //layout
+#define P_UNI 3//unicode method
+#define P_STR 4 //string to type
+#define NUM_P P_STR+1 //number of parameters
+
 void send_key (FILE* hid_dev, unsigned short key, unsigned short mod);
 
 int main (int argc, char** argv) {
-	if (argc != 4) {
-		fprintf (stderr, "Usage: %s <device file> <layout> \"<string>\"\n", argv[0]);
+	if (argc != NUM_P) {
+		fprintf (stderr, "Usage: %s <device file> <layout> <unicode> \"<string>\"\n", argv[P_EXE]);
 		return 1;
 	}
 	FILE* hid_dev = fopen ("/dev/hidg0", "w");
-	for (int i = 0; i < strlen (argv[3]); i++) {
+	for (int i = 0; i < strlen (argv[P_STR]); i++) {
 
-		char tmp[UTF8_MAX_LENGTH] = {argv[3][i], argv[3][i+1], argv[3][i+2], '\0'};
+		char tmp[UTF8_MAX_LENGTH] = {argv[P_STR][i], argv[P_STR][i+1], argv[P_STR][i+2], '\0'};
 		//TODO: replace by something less stupid
-		if (argv[3][i] < 128) { // not multi byte
+		if (argv[P_STR][i] < 128) { // not multi byte
 			tmp[1] = '\0';
 		} else { // is multi byte
-			if (argv[3][i] < 0xe0) {
+			if (argv[P_STR][i] < 0xe0) {
 			i++; //skip next thing
 			tmp[2] = 0;
 			} else {
@@ -38,24 +47,58 @@ int main (int argc, char** argv) {
 			fprintf (stderr, "Key Symbol not found.\n");
 			return 1;
 		}
-		switch (atoi (argv[2])) {
+		struct layout* l;
+		int ignore_deadkey = 0;
+		switch (atoi (argv[P_LAY])) {
 		case 0:
 			fprintf (stderr, "This keyboard layout is reserved.\n");
 			return 1;
 		case 1: //en_us
-			send_key(hid_dev, s->en_us.key, s->en_us.mod);
+			l = &(s->en_us);
 			break;
 		case 2: //de_at
-			send_key(hid_dev, s->de_at.key, s->de_at.mod);//, s->de_at.is_dead);
+			l = &(s->de_at);
 			break;
 		case 3: //de_at-nodeadkeys
-			send_key(hid_dev, s->de_at.key, s->de_at.mod);
+			l = &(s->de_at);
+			ignore_deadkey = 1;
 			break;
 		default:
 			fprintf (stderr, "Unrecognised keyboard layout.\n");
 			return 1;
 		}
-		send_key(hid_dev, '\0', '\0');
+		if (l->key == 0x00) {
+			//key does not exist in this layout
+			fprintf (stderr, "Key not in this layout!\n");
+			/*TODO: send unicode sequence
+			  there are different methods to be used for gtk and
+			  winblows. ctrl-shift-u-HEX vs. ctrl-shift-u,HEX,SPACE
+			  vs. alt+NUMPAD vs. alt+'+'+HEX
+			*/
+			switch (atoi (argv[P_UNI])) {
+			case 0: //skip unicode character entry
+				break;
+			case 1: //gtk: hold ctrl and shift while entering
+				//TODO
+				break;
+			case 2: //gtk: use space as end marker for unicode
+				//TODO
+			case 3: //windows: alt+numpad (decimal)
+				//TODO
+				break;
+			default:
+				fprintf (stderr, "Unicode Method unknown!\n");
+				return 1;
+			}
+		} else {
+			send_key(hid_dev, l->key, l->mod);
+			send_key(hid_dev, '\0', '\0'); //release all keys
+			if (l->is_dead && !ignore_deadkey) {
+				//dead keys need to be pressed twice to show up
+				send_key(hid_dev, l->key, l->mod);
+				send_key(hid_dev, '\0', '\0'); //release all keys
+			}
+		}
 	}
 	fclose (hid_dev);
 
